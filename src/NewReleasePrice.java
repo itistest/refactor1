0c64b07fc5823c7945ed19bedf28ca4026f46702
john <759547022@qq.com>
2016-07-21 00:07:11 +0800

    不把超类函数声明为，为新片类型增加一个覆写函数，并在超类中留下一个已定义的函数，使它成为一种默认行为

--- src/NewReleasePrice.java
+++ src/NewReleasePrice.java
@@ -9,4 +9,10 @@ public class NewReleasePrice extends Price {
 	double getCharge(int daysRented) {
 			return daysRented * 3;
 	}
+
+	@Override
+	int getFrequenterPoints(int daysRented) {
+		return (daysRented > 1)? 2 : 1;
+	}
+	
 }
--- src/Price.java
+++ src/Price.java
@@ -2,9 +2,6 @@ public abstract class Price {
 	abstract int getPriceCode();
 	abstract double getCharge(int daysRented);
 	int getFrequenterPoints(int daysRented) {
-		if ((getPriceCode() == Movie.NEW_RELEASE) && daysRented > 1) {
-			return 2;
-		} else
-			return 1;
+		return 1;
 	}
 }
